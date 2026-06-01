dd837fe7b9d6e582e429d480644d1d2b059b1eea
Dmitrii Chizha <me@filaco.dev>
2026-06-01 17:45:13 +0300

    feat(mailcow): update cert script and systemd service

--- nginx-proxy-manager/scripts/mailcow-cert-update.sh
+++ nginx-proxy-manager/scripts/mailcow-cert-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp $NGINX_PROXY_MANAGER_HOME/letsencrypt/live/npm-1/fullchain.pem /home/filaco/Projects/containers/mailcow-dockerized/data/assets/ssl/cert.pem
-cp $NGINX_PROXY_MANAGER_HOME/letsencrypt/live/npm-1/privkey.pem /home/filaco/Projects/containers/mailcow-dockerized/data/assets/ssl/key.pem
+cp /mnt/nginx-proxy-manager/letsencrypt/live/npm-1/fullchain.pem /home/filaco/Projects/containers/mailcow-dockerized/data/assets/ssl/cert.pem
+cp /mnt/nginx-proxy-manager/letsencrypt/live/npm-1/privkey.pem /home/filaco/Projects/containers/mailcow-dockerized/data/assets/ssl/key.pem
 postfix_c=$(docker ps -qaf name=postfix-mailcow)
 dovecot_c=$(docker ps -qaf name=dovecot-mailcow)
 nginx_c=$(docker ps -qaf name=nginx-mailcow)
